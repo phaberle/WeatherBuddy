42f4d3afba11ad522892856918f21323060dbb35
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-05-01 21:03:51 -0500

    got the UX and data to work together

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WeatherBuddy
-WeatherBuddy
+## Introduction
+<p>
+WeatherBuddy is an application that reports current weather and also gives a 5-day forecast for US cities. It gets its information from OpenWeather API. Current weather is at the top of the page, and the forecast is beneath that. Eight of the past searches will be recorded on the left side as buttons. This is an assignment from the UT Austin coding boot camp.
+</p>
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,25 @@
-const apiKey = "990fb7b2f72e24a5eaca77e18553684b";
+var searchBtn = document.querySelector("#save");
+var userSrch = document.querySelector("#userTxt");
+var histSec = document.querySelector(".historySection");
 
+
+const apiKey = "990fb7b2f72e24a5eaca77e18553684b";
 var getCityStateLocation = function(city, state) {
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + ",US&limit=1&appid=" + apiKey)
         .then(function(response) {
             //request was successful
             if (response.ok) {
                 response.json().then(function(data) {
-                    //console.log(data.length);
                     if (data) {
                         var lat = data[0].lat;
                         var lon = data[0].lon;
                         var city = data[0].name;
                         var state = data[0].state;
                         var coorArray = [lat, lon, city, state];
-
-                        //console.log(coordinates);
+                        debugger;
                         getWeatherFromCoordinates(coorArray);
                     } else {
-                        // alert("Error: Nothing found."); << ADD DOM ELEMENT ALERT
+                        window.alert("Error: Nothing found.");
                     }
                 });
             } else {}
@@ -53,40 +55,96 @@ var getWeatherFromCoordinates = function(coorArray) {
                         CurrentWeather.uv = data.current.uvi;
                         CurrentWeather.wind = data.current.wind_speed + " MPH";
                         CurrentWeather.humidity = data.current.humidity + "%";
-                        // console.log("City: " + CurrentWeather.city);
-                        // console.log("State: " + CurrentWeather.state);
-                        // console.log("IconLnk: " + CurrentWeather.icon);
-                        // console.log("Today's Date: " + CurrentWeather.date);
-                        // console.log("Current Temp: " + CurrentWeather.temp);
-                        // var dailyObjects = data.daily;
 
                         for (let i = 1; i <= 5; i++) {
                             DailyWeather.date.push(epochtoHuman(data.daily[i].dt));
-                            DailyWeather.temp.push(`${formatTemp(data.daily[i].temp.min)}/${formatTemp(data.daily[i].temp.max)}`);
+                            DailyWeather.temp.push(`${formatTemp(data.daily[i].temp.max)} ${formatTemp(data.daily[i].temp.min)}`);
                             DailyWeather.uvi.push(data.daily[i].uvi);
                             DailyWeather.humidity.push(data.daily[i].humidity + "%");
                             DailyWeather.icon.push("http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
                             DailyWeather.wind.push(data.daily[i].wind_speed + " MPH");
                         }
+
                     } else {
-                        // alert("Error: Nothing found."); << ADD DOM ELEMENT ALERT
+                        window.alert("Error: Nothing found.");
                     }
+                    buildCurrentWeatherPart(CurrentWeather);
+                    buildDailyWeatherPart(DailyWeather);
                 });
             } else {}
         })
         .catch(function(error) {
-            //Notice this '.catch()' getting chained onto the end of the .'then()' method
             alert("Unable to connect to geoLocationsFile");
         });
 }
 
 
+var buildCurrentWeatherPart = function(CurrentWeather) {
+    var city_state = CurrentWeather.city + "," + CurrentWeather.state;
+    var uviColorCoding = UVtoColorCoding(CurrentWeather.uv);
+    document.getElementById("currentWeather").innerHTML = `
+    <div id="currWeatherHeadline">
+    <h3>${CurrentWeather.city}, ${CurrentWeather.state} (${CurrentWeather.date}) <img id="currWeatherIcon" src="${CurrentWeather.icon}"></h3>
+    <ul id="currLst">
+    <li>Temp: ${CurrentWeather.temp}</li>
+    <li>Wind: ${CurrentWeather.wind}</li>
+    <li>Humidity: ${CurrentWeather.humidity}</li>
+    <li id="uvItem">UV Index: <span style="background:${uviColorCoding}; margin-left:4px;text-align:center;padding:3px;border:1px black solid"> ${CurrentWeather.uv}<span></li>
+    </ul>
+    `
+    var histSecCt = histSec.childElementCount;
+    var histParent = histSec;
+    if (histSecCt == 8) {
+        if (window.confirm("Max number of history buttons reached.\nI have to clear them.")) {
+            document.querySelector(".historySection").innerHTML = "";
+            var newBtn = document.createElement("button");
+            newBtn.setAttribute("class", "histBtn");
+            newBtn.setAttribute("type", "button");
+            newBtn.setAttribute("data-cityState", city_state);
+            newBtn.textContent = city_state;
+            histParent.appendChild(newBtn);
+        }
+    } else {
+
+        var newBtn = document.createElement("button");
+        newBtn.setAttribute("class", "histBtn");
+        newBtn.setAttribute("type", "button");
+        newBtn.setAttribute("data-cityState", city_state);
+        newBtn.textContent = city_state;
+        histParent.appendChild(newBtn);
+    }
+}
+var buildDailyWeatherPart = function(DailyWeather) {
+    var content = ""
+    document.getElementById("dCardContainer").innerHTML = "";
+    for (var i = 0; i < 5; i++) {
+        var dailyUVI = UVtoColorCoding(DailyWeather.uvi[i]);
+        content =
+            `
+        <div class="dCard">
+        <ul>
+      <li>${DailyWeather.date[i]}</l>
+      <li><img src="${DailyWeather.icon[i]}"></li>
+      <li>Temp: ${DailyWeather.temp[i]}</li>
+      <li>Wind: ${DailyWeather.wind[i]}</li>
+      <li>Humidity: ${DailyWeather.humidity[i]}</li>
+      <li id="uvItem">UV Index: <span style="background:${dailyUVI}; margin-left:4px;text-align:center;padding:3px;border:1px black solid; color:black"> ${DailyWeather.uvi[i]}<span></li>
+        </ul>
+        </div>
+        `;
+        document.getElementById("dCardContainer").innerHTML += content;
+    }
+}
+
+
+
 var UVtoColorCoding = function(UVNum) {
     let answer = "";
-    if (UVNum <= 2) { answer = "green" };
-    if (UVNum >= 3 && UVNum <= 5) { answer = "yellow" };
-    if (UVNum >= 6 && UVNum <= 7) { answer = "orange" };
-    if (UVNum >= 8 && UVNum <= 10) { answer = "red" };
+    if (UVNum <= 2.99) { answer = "green" };
+    if (UVNum >= 3.00 && UVNum <= 5.99) { answer = "yellow" };
+    if (UVNum >= 6.00 && UVNum <= 7.99) { answer = "orange" };
+    if (UVNum >= 8.00 && UVNum <= 10.99) { answer = "red" };
+    if (UVNum > 10.99) { answer = "red" };
     return answer;
 }
 
@@ -98,7 +156,16 @@ function formatTemp(temp) {
     return Math.round(temp);
 }
 
-function clearDailyWeatherArrays() {
+function clearWeatherDatasets() {
+    CurrentWeather.city = "";
+    CurrentWeather.state = "";
+    CurrentWeather.icon = "";
+    CurrentWeather.date = "";
+    CurrentWeather.temp = "";
+    CurrentWeather.uv = "";
+    CurrentWeather.wind = "";
+    CurrentWeather.humidity = "";
+
     DailyWeather.date.splice(0, DailyWeather.date.length);
     DailyWeather.temp.splice(0, DailyWeather.temp.length);
     DailyWeather.uvi.splice(0, DailyWeather.uvi.length);
@@ -107,4 +174,41 @@ function clearDailyWeatherArrays() {
     DailyWeather.wind.splice(0, DailyWeather.wind.length);
 }
 
-getCityStateLocation("Dallas", "Tx");
+handleSearch = function(event) {
+    event.preventDefault();
+    var userInput = userSrch.value;
+    userSrch.value = "";
+    var txtArray = userInput.split(',');
+    if (txtArray.length == 2) {
+        let city = txtArray[0];
+        let state = txtArray[1];
+        clearWeatherDatasets();
+        getCityStateLocation(city, state);
+    } else {
+        window.alert("Please enter city, state for search.");
+    }
+}
+
+
+handleHistoryRecall = function(cityState) {
+    var txtArray = cityState.split(',');
+    if (txtArray.length == 2) {
+        let city = txtArray[0];
+        let state = txtArray[1];
+        clearWeatherDatasets();
+        getCityStateLocation(city, state);
+    } else {
+        window.alert("Please enter city, state for search.");
+    }
+}
+
+var recallHistory = function(event) {
+    var cityState = event.target.getAttribute("data-cityState");
+    handleHistoryRecall(cityState);
+}
+
+
+
+
+searchBtn.addEventListener("click", handleSearch);
+histSec.addEventListener("click", recallHistory);
